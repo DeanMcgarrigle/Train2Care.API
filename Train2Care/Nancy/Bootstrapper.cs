2f9645d1a7d7035037434f50bde2b3b4392a5713
DeanMcgarrigle <dean.mcgarrigle@gmail.com>
2018-04-12 16:24:43 +0100

    Initial

---
+++ Train2Care/Nancy/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace Train2Care.Nancy
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("build"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("app/assets/img"));
+        }
+    }
+}
---
+++ Train2Care/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin.Extensions;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Nancy.Owin;
+using Owin;
+using Train2Care.Nancy;
+
+namespace Train2Care
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var physicalFileSystem = new PhysicalFileSystem(@".\"); //. = root, Web = your physical directory that contains all other static content, see prev step
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            app.UseFileServer(options);
+
+            app.UseNancy(NancyConfig);
+            app.UseStageMarker(PipelineStage.MapHandler);
+
+            //FluentMapper.Initialize(config =>
+            //{
+            //    config.AddMap(new CustomerDetailsModelMapper());
+            //    config.AddMap(new SearchModelMapper());
+            //    config.AddMap(new CheckBoxMapper());
+            //});
+        }
+
+        private static void NancyConfig(NancyOptions nancyOptions)
+        {
+            nancyOptions.Bootstrapper = new Bootstrapper();
+        }
+    }
+}
